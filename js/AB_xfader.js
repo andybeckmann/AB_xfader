316a799aa00360414e7c8c0f5bc8873769ec2910
Andy Beckmann <abec19@gmail.com>
2020-02-16 11:54:40 -0500

    Clean up comments

--- js/AB_xfader.js
+++ js/AB_xfader.js
@@ -12,62 +12,54 @@ function xfader(delay) {
     var transitionDelay = 0;
 
     if (delay > 0) {
-        // Set custom delay in seconds
         transitionDelay = delay;
     } else {
-        // Set default value of 10 seconds
         transitionDelay = 10;
     }
 
-    // Set fader variables
     var transitionTime = transitionDelay * 1000,
         currentImageNumber = 0,
         itemCount = document.getElementsByClassName('item').length;
 
-    // Run nextImage() after transitionTime
     var faderTransition = setInterval(fader, transitionTime);
 
-    // Function: nextImage
+    /**
+     * Function: fader
+     */
     function fader() {
 
-        // Remove currentFade class from current image
         var element;
         element = document.getElementById('slide-' + currentImageNumber);
         fadeOut(element, 'currentFade');
-        
-        // Calculate next currentImageNumber
+
         currentImageNumber++;
 
-        // Reset currentImageNumber
         if (currentImageNumber > itemCount - 1) {
             currentImageNumber = 0;
         }
-        
-        // Add currentFade class to next image
+
         element = document.getElementById('slide-' + currentImageNumber);
         fadeIn(element, 'currentFade');
     }
      
-    // Function: fadeIn   
+    /**
+     * Function: fadeIn
+     */  
     function fadeIn(element, name) {
 
-        // Set classList from element
         var classList = element.className;
 
-        // Add name to classList
         classList += ' ' + name;
 
-        // Update elements class list
         element.className = classList;
     }
 
-    // Functionn: fadeOut
+    /**
+     * Function: fadeOut
+     */
     function fadeOut(element, name) {
 
-        // Get classList from element
         var classList = element.className;
-
-        // Remove class from list
         element.className = classList.replace(' ' + name, '');
     }
 }
